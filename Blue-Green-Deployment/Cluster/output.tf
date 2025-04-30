880ac9e0ee3afce9ac05adbb2cf46958e6f02acf
Ubuntu <ubuntu@ip-172-31-20-10.ec2.internal>
2025-04-30 08:22:05 +0000

    adding task code

---
+++ Blue-Green-Deployment/Cluster/output.tf
@@ -0,0 +1,16 @@
+output "cluster_id" {
+  value = aws_eks_cluster.hkoti.id
+}
+
+output "node_group_id" {
+  value = aws_eks_node_group.hkoti.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.hkoti_vpc.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.hkoti_subnet[*].id
+}
+
